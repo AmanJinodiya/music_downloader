e1fd8904cddc2207a184f2b893c72901c8685df0
Aman <amanjinodiya@gmail.com>
2021-08-11 11:02:05 +0530

    added function to download multiple songs

--- music_downloader.py
+++ music_downloader.py
@@ -19,57 +19,57 @@ def speak(comm):
 
 def click():
 
-# Create object
-    driver = webdriver.Chrome()
+    list = (song_n.get()).split("-")
+    for item in list:
+        driver = webdriver.Chrome()
 
-# Assign URL
-    url = "https://mp3quack.lol/"
+        url = "https://mp3quack.lol/"
 
-# New Url
-    new_url = "https://mp3quack.lol/"
 
-# Opening first url
-    driver.get(url)
+        new_url = "https://mp3quack.lol/"
 
-    inputElems = driver.find_elements_by_css_selector('[id="searchInput"]')
 
-    for inputElem in inputElems:
+        driver.get(url)
 
-	    inputElem.send_keys(song_n.get())
+        inputElems = driver.find_elements_by_css_selector('[id="searchInput"]')
 
-	# Presses Enter Key Like When You Press Enter Key to Search
-	    inputElem.send_keys(Keys.ENTER)
+        for inputElem in inputElems:
 
-    down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
+	        inputElem.send_keys(item)
 
-# Open a new window
-    driver.execute_script("window.open('');")
+	
+	        inputElem.send_keys(Keys.ENTER)
 
-# Switch to the new window and open new URL
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(new_url)
+        down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
 
-    inputElems = driver.find_elements_by_css_selector('[id="searchInput"]')
 
-    for inputElem in inputElems:
+        driver.execute_script("window.open('');")
 
-	    inputElem.send_keys(song_n.get())
 
-	# Presses Enter Key Like When You Press Enter Key to Search
-	    inputElem.send_keys(Keys.ENTER)
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(new_url)
+    
+        inputElems = driver.find_elements_by_css_selector('[id="searchInput"]')
 
-    down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
-    down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
-    speak("sir whichever you want click on it please or I choose for you ")
-    time.sleep(5)
-    final_click = driver. find_element_by_xpath('//*[@id="downloadBtn"]').click()
+        for inputElem in inputElems:
+
+	        inputElem.send_keys(item)
+
+	
+	        inputElem.send_keys(Keys.ENTER)
+
+        down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
+        down_click  = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[2]/ul[1]/li[3]/span[2]").click()
+        speak("sir whichever you want click on it please")
+        time.sleep(10)
+    
 
 
 root = Tk()
 
 root.geometry("700x700")
 root.title("MuSiC DoWNlodER")
-root.wm_iconbitmap("img1.ico")
+# root.wm_iconbitmap("img1.ico")
 root.config(bg="black")
 
 img = Image.open("bg4.png")
@@ -79,6 +79,7 @@ Label(root, image= photo).place(x = 0, y = 0 )
 
 Label(root, text= "MUSIC == LOVE ", font = "lucida 30 bold", fg="salmon",bg ="dark slate gray").place(x =190, y = 10 )
 song_n= StringVar()
+Label(root, text= "If you want to download multiple songs then put - between every song", font ="lucida 15 bold").place(x = 15, y = 660)
 
 # a =Text(root,height=1,width= 50, font = "lucida 10 bold")
 # a
